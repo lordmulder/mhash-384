7599c58a0a214d6f1fb1072cb196083678bebdb7
LoRd_MuldeR <mulder2@gmx.de>
2018-01-07 23:47:49 +0100

    Small improvement.

--- bindings/Python/start.py
+++ bindings/Python/start.py
@@ -6,5 +6,5 @@ def self_test():
     MHashPy384.MHash384.self_test()
 
 print('MHashPy384 v{}.{}-{}'.format(*MHashPy384.MHash384.version()))
-print('Running on {}, v{}.{}.{}-{}\n'.format(sys.implementation.name, sys.version_info[0], sys.version_info[1], sys.version_info[2], sys.version_info[3]))
+print('Running on {}, v{}.{}.{}-{}\n'.format(sys.implementation.name, *sys.version_info))
 print('\nTook %.2f seconds.' % timeit.timeit(self_test, number=1))
