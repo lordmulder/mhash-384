87379c093bdb6111b2ab8cb5198ed4513e3903d3
LoRd_MuldeR <mulder2@gmx.de>
2018-01-11 20:49:34 +0100

    Improved Python startup script.

--- bindings/Python/start.py
+++ bindings/Python/start.py
@@ -1,10 +1,16 @@
 import sys
+
+if sys.version_info[0] < 3:
+   raise SystemError("Python 3.x or newer is required to run this application !!!")
+
 import timeit
-import MHashPy384
+import MHashPy384 as mhash384
 
-def self_test():
-    MHashPy384.MHash384.self_test()
+print('MHashPy384 v{}.{}-{}'.format(*mhash384.MHash384.version()), end=' ')
+print('[on {} v{}.{}.{}-{}]\n'.format(sys.implementation.name, *sys.implementation.version))
 
-print('MHashPy384 v{}.{}-{}'.format(*MHashPy384.MHash384.version()))
-print('Running on {}, v{}.{}.{}-{}\n'.format(sys.implementation.name, *sys.version_info))
-print('\nTook %.2f seconds.' % timeit.timeit(self_test, number=1))
+if sys.implementation.name.lower() == "cpython":
+    print("WARNING: You are running on CPython, which is VERY SLOW for actual computations!")
+    print("You can use, for example, PyPy (https://pypy.org/) for *much* improved speed ;-)\n")
+
+print('\nTook %.2f seconds.' % timeit.timeit(mhash384.MHash384.self_test, number=1))
